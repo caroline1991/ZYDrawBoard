b2597abc23a38243effb8117f5d4b768e5460aae
王志盼 <1204473090@qq.com>
2018-05-31 12:02:46 +0800

    Initial Commit

---
+++ ZYDrawBoard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZYDrawBoard
+//
+//  Created by 王志盼 on 2018/5/31.
+//  Copyright © 2018年 王志盼. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
